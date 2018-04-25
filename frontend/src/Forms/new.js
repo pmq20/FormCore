2c20b3dbc55b0ecbbc9727f6cd5a6a4899bbf346
Minqi Pan <pmq2001@gmail.com>
2018-04-25 15:48:07 +0800

    frontend: new form form

--- frontend/src/Forms/new.js
+++ frontend/src/Forms/new.js
@@ -1,14 +1,81 @@
-import React, { Fragment } from 'react';
-import { Route, Link, Switch, Redirect } from 'react-router-dom';
-import { Card } from 'antd';
+import React, { PureComponent } from 'react';
+import {
+  Form,
+  Input,
+  DatePicker,
+  Select,
+  Button,
+  Card,
+  InputNumber,
+  Radio,
+  Icon,
+  Tooltip,
+} from 'antd';
+import styles from './new.less';
 
-export default class FormsNew extends React.Component {
+const FormItem = Form.Item;
+const { Option } = Select;
+const { RangePicker } = DatePicker;
+const { TextArea } = Input;
+
+@Form.create()
+export default class FormsNew extends PureComponent {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        this.props.dispatch({
+          type: 'form/submitRegularForm',
+          payload: values,
+        });
+      }
+    });
+  };
   render() {
+    const { submitting } = this.props;
+    const { getFieldDecorator, getFieldValue } = this.props.form;
+
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 7 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 12 },
+        md: { span: 10 },
+      },
+    };
+
+    const submitFormLayout = {
+      wrapperCol: {
+        xs: { span: 24, offset: 0 },
+        sm: { span: 10, offset: 7 },
+      },
+    };
+
     return (
       <Card
         title="New Form"
         style={{ marginBottom: 24 }}
       >
+        <Form onSubmit={this.handleSubmit} hideRequiredMark style={{ marginTop: 8 }}>
+          <FormItem {...formItemLayout} label="Form Title">
+            {getFieldDecorator('title', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please enter the form title',
+                },
+              ],
+            })(<Input placeholder="Enter a form title" />)}
+          </FormItem>
+          <FormItem {...submitFormLayout} style={{ marginTop: 32 }}>
+            <Button type="primary" htmlType="submit" loading={submitting}>
+              Submit
+            </Button>
+          </FormItem>
+        </Form>
       </Card>
     );
   }
