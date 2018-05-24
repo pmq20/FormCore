6f5365f971c70732a97ab585c9ecaca0d6417129
Minqi Pan <pmq2001@gmail.com>
2018-05-24 12:53:08 +0900

    v0.5.5 -- fix Frontend styles

--- Frontend/src/FooterToolbar/index.jsx
+++ Frontend/src/FooterToolbar/index.jsx
@@ -6,8 +6,8 @@ export default class FooterToolbar extends Component {
     const { children, className, extra, ...restProps } = this.props;
     return (
       <div className={classNames(className, 'formcore_footertoolbar')} {...restProps}>
-        <div className={styles.left}>{extra}</div>
-        <div className={styles.right}>{children}</div>
+        <div className="formcore_left">{extra}</div>
+        <div className="formcore_right">{children}</div>
       </div>
     );
   }
