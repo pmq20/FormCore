5a0e70d5d7650d7f1a37858fa11f1a1f01b91cc6
Minqi Pan <pmq2001@gmail.com>
2018-06-05 16:53:00 +0800

    Backend: add Input

---
+++ Backend/Input/FForm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormCore {
+  public class FForm {
+    public int ParentId { get; set; }
+    public string Title { get; set; }
+  }
+}
