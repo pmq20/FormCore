ae4c33f0797d769159f7a79c2d8c40a92999adbe
Minqi Pan <pmq2001@gmail.com>
2018-08-02 11:20:49 +0800

    let OForm class derivative provide Parents themselves

--- Backend/Output/OForm.cs
+++ Backend/Output/OForm.cs
@@ -11,16 +11,11 @@ namespace FormCore {
       Title = form.Title;
       Sections = form.AllSections(db).Select(x => new OSection(x)).ToList();
       Fields = form.AllFields(db).Select(x => new OField(x)).ToList();
-      Parents = form.Parents.Select(x => new OForm {
-        Id = x.Id,
-        Title = x.Title,
-      }).ToList();
   }
 
     public int Id { get; set; }
     public string Title { get; set; }
     public List<OSection> Sections { get; set; }
     public List<OField> Fields { get; set; }
-    public List<OForm> Parents { get; set; }
   }
 }
